ef9b4606e99c57b3d340367f69e6ff31bf53d136
Howon Lee <howon@howonlee.com>
2015-08-02 16:58:01 -0700

    op functionality exists

--- op.py
+++ op.py
@@ -1,11 +1,32 @@
 import sys
+from subprocess import call
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print "put in an argument"
         sys.exit(1)
-    filename = sys.argv[1]
-    if filename[:4] == ".pdf":
-        put out evince for this shit
-    elif filename[:4] in [".jpg", ".png", ".gif", other shit]:
-        put out feh for this shit
+    for filename in sys.argv[1:]:
+        ext = filename.rpartition(".")[-1]
+        print "found a ", ext, " file. Trying to open..."
+        if ext in ["pdf"]:
+            print "opening pdf..."
+            call(["evince", filename])
+        elif ext in ["jpeg", "jpg", "png"]:
+            print "opening picture with feh..."
+            call(["feh", filename])
+        elif ext in ["gif"]:
+            print "opening gif with gifview..."
+            call(["gifview", filename])
+        elif ext in ["svg"]:
+            print "opening svg with inkscape..."
+            call(["inkscape", filename])
+        elif ext in ["doc", "docx", "xls", "xlsx", "ppt",
+                "pptx", "rtf", "odt", "fodt", "ods", "fods",
+                "odp", "fodp", "odb", "odg", "fodg", "odf"]:
+            print "opening officey format with libreoffice..."
+            call(["libreoffice", filename])
+        elif ext in ["wav", "mp3", "ogg"]:
+            print "opening sound file with mplayer..."
+            call(["mplayer", filename])
+        else:
+            print "I don't recognize this file"
---
+++ readme.md
@@ -0,0 +1,6 @@
+op
+===
+
+Not anything to do with, like, 4chan op
+
+just because I was annoyed with picking between ev and feh whenever I had a file of some sort
